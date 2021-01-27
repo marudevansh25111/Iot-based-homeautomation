0852da9da3144886ab5d57dda4d07fce7cc169f9
gauravMori158 <77385932+gauravMori158@users.noreply.github.com>
2021-01-27 22:54:49 +0530

    add home automation

---
+++ Home/templates/templates/info.php
@@ -0,0 +1,14 @@
+
+
+<html>
+
+
+<body>
+
+<form>
+    <h1><p style="color:red"> IOT Based Automation</p></h1>
+   <label> welcome {{n}}</label>
+ 
+</form>
+    </body>
+    </html>
---
+++ Home/urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls import url
+from Home import views
+urlpatterns = [
+url(r'^', views.HomePageView.as_view()),
+]
