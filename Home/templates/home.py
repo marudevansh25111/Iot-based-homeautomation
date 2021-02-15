9059b2c4e63678960392b443218086b5d424c42e
gauravMori158 <77385932+gauravMori158@users.noreply.github.com>
2021-02-15 18:34:13 +0530

    Adding With new modules

--- Home/templates/home.py
+++ Home/templates/home.py
@@ -1,7 +1,7 @@
 #!C:/Users/Gaurav Mori/AppData/Local/Microsoft/WindowsApps/python.exe
+import cgi
 print("Content-type: text/html")
 print()
-import cgi
 
 
 form =cgi.FieldStorage()
@@ -9,6 +9,11 @@ name1=str(form.getvalue('name'))
 passw=str(form.getvalue('password'))
 
 if name1=="user1" and passw=="user1":
-    print ("welcome ",name1)
+     redirectURL = "http://169.254.81.214:1880/ui/#!/0?socketid=NiUOP-obSz3agwMRAAAE"
+     print('<html>')
+     print('  <head> ')
+     print('    <meta http-equiv="refresh" content="0;url='+str(redirectURL)+'" />') 
+     print(' <title>IOT Home Automation</title> </head>')
+     print('</html>')
 else:
     print("Enter valid username and password")
