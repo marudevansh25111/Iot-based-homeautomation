43690b8f79e52c6386509addf6c7ac3e862c3c8c
gauravMori158 <77385932+gauravMori158@users.noreply.github.com>
2021-02-23 10:29:30 +0530

    add file

--- Home/templates/home.py
+++ Home/templates/home.py
@@ -7,13 +7,23 @@ print()
 form =cgi.FieldStorage()
 name1=str(form.getvalue('name'))
 passw=str(form.getvalue('password'))
+opt=str(form.getvalue('iot'))
 
 if name1=="user1" and passw=="user1":
      redirectURL = "http://169.254.81.214:1880/ui/#!/0?socketid=NiUOP-obSz3agwMRAAAE"
+     if opt=="dash" :
+           redirectURL = "http://169.254.81.214:1880/ui/#!/0?socketid=NiUOP-obSz3agwMRAAAE"
+     else :
+           redirectURL="http://169.254.81.214:1880/#flow/c20877c4.54baa8"
+
      print('<html>')
      print('  <head> ')
+   
      print('    <meta http-equiv="refresh" content="0;url='+str(redirectURL)+'" />') 
+     print("  <link rel = 'icon' href = 'img/pi.png' type = 'image/x-icon'> ")
      print(' <title>IOT Home Automation</title> </head>')
      print('</html>')
 else:
-    print("Enter valid username and password")
+      print(' <title>IOT Home Automation</title> </head>')
+      print("  <link rel = 'icon' href = 'img/pi.png' type = 'image/x-icon'> ")
+      print("Enter valid username and password")
